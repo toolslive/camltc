39396e860b20728e29dc1aae423824488368bd39
Romain Slootmaekers <romain.slootmaekers@openvstorage.com>
2016-10-21 09:30:50 +0200

    fix demo

--- example/demo.ml
+++ example/demo.ml
@@ -1,8 +1,8 @@
 open Lwt
 
-let main () = 
-  let t = 
-    Camltc.Hotc.create "/tmp/bla.db" >>= fun db ->
+let main () =
+  let t =
+    Camltc.Hotc.create "/tmp/bla.db" [] >>= fun db ->
     Camltc.Hotc.close db
   in
   Lwt_main.run t;;
