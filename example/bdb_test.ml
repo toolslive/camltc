fd30e63168aa7b40ffaf02f37ee62917439c739b
Francois BERENGER <unixjunkie@sdf.org>
2018-07-18 11:41:21 +0900

    test operations using a cursor

--- example/bdb_test.ml
+++ example/bdb_test.ml
@@ -1,3 +1,6 @@
+open Printf
+
+module Bdb = Camltc.Bdb
 
 let nuke db fn =
   Camltc.Hotc.bdb_close db;
@@ -7,7 +10,23 @@ let nuke db fn =
 let () =
   let fn = "/tmp/test.tc" in
   let db = Camltc.Hotc.bdb_create fn [] in
-  Camltc.Bdb.put db "key" "value";
-  assert(Camltc.Bdb.get db "key" = "value");
-  Printf.printf "OK\n";
+  Bdb.put db "key" "value";
+  Bdb.put db "key0" "value0";
+  Bdb.put db "key1" "value1";
+  Bdb.put db "key2" "value2";
+  assert(Bdb.get db "key" = "value");
+  let cur = Camltc.Hotc.bdb_get_cursor db in
+  Bdb.last db cur;
+  assert(Bdb.key db cur = "key2");
+  assert(Bdb.value db cur = "value2");
+  let n = Int64.to_int (Bdb.get_key_count db) in
+  Bdb.first db cur;
+  for i = 1 to n do
+    let k = Bdb.key db cur in
+    let v = Bdb.value db cur in
+    printf "%s:%s\n" k v;
+    if i < n then
+      Bdb.next db cur
+  done;
+  printf "OK\n";
   nuke db fn
