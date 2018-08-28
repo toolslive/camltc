89befa5e61d90b07f66d937322b767bdd23a1009
Francois BERENGER <unixjunkie@sdf.org>
2018-08-28 15:47:58 +0900

    put back some ocamldoc from a previous PR

--- src/camltc.mli
+++ src/camltc.mli
@@ -28,8 +28,15 @@ module Bdb : sig
   val default_mode : int
   val readonly_mode : int
   type opt = BDBTLARGE
+
+  (** [put db key value] add to [db] the [key] to [value] binding. *)
   val put : bdb -> string -> string -> unit
+
+  (** [get db key] retrieve from [db] the value associated with [key].
+      @raise DOCUMENT_ME in case [key] was not found in [db]. *)
   val get: bdb -> string -> string
+
+  (** [get3 db key] DOCUMENT_ME *)
   val get3: bdb -> string -> string
 
   val oreader: int
@@ -61,14 +68,22 @@ module Bdb : sig
       as the key for a get3
   *)
 
+  (** [get_nolock db key] DOCUMENT_ME probably faster version
+      of get on a readonly-opened database *)
   val get_nolock : bdb -> string -> string
+
+  (** [out db key] remove from [db] the binding for [key].
+      @raise DOCUMENT_ME in case [key] was not found in [db]. *)
   val out: bdb -> string -> unit
+
   val range: bdb ->
     string option -> bool ->
     string option -> bool -> int ->
     string array
 
+  (** [exists db key] check if [key] is bound in [db]. *)
   val exists : bdb -> string -> bool
+
   val delete_prefix : bdb -> string -> int
   val prefix_keys : bdb -> string -> int -> string array
 
@@ -95,19 +110,44 @@ module Bdb : sig
     bool -> string option -> bool -> int -> (string * string) list
   val get_key_count : bdb -> int64
 
+  (** [first db cur] place cursor [cur] in front of the first
+      record in [db]. *)
   val first: bdb -> bdbcur -> unit
-  val jump : bdb -> bdbcur -> string -> unit
-  val next : bdb -> bdbcur -> unit
-  val prev : bdb -> bdbcur -> unit
-  val last : bdb -> bdbcur -> unit
-  val cur_out  : bdb -> bdbcur -> unit
-  (** [cur_out b c] deletes the current key value pair and jumps to next *)
 
+  (** [jump db cur key] place cursor [cur] in front of the
+      record with key [key] in [db]. *)
+  val jump : bdb -> bdbcur -> string -> unit
+
+  (** [next db cur] place cursor [cur] in front of the next
+      record in [db] *)
+  val next : bdb -> bdbcur -> unit
+
+  (** [prev db cur] place cursor [cur] in front of the previous
+      record in [db].
+      DOCUMENT_ME what does it do when we were already on the first? *)
+  val prev : bdb -> bdbcur -> unit
+
+  (** [last db cur] place cursor [cur] in front of the last
+      record in [db] *)
+  val last : bdb -> bdbcur -> unit
+
+  (** [cur_out db cur] delete the key value pair pointed to by [cur]
+      in [db] and go to the next one *)
+  val cur_out  : bdb -> bdbcur -> unit
+
+  (** [key db cur] get the key currently pointed to by cursor [cur] *)
   val key: bdb -> bdbcur -> string
+    
+  (** [value db cur] get the value currently pointed to by cursor [cur] *)
   val value: bdb -> bdbcur -> string
+
+  (** [key3 db cur] DOCUMENT_ME *)
   val key3: bdb -> bdbcur -> string
+
+  (** [value3 db cur] DOCUMENT_ME *)
   val value3: bdb -> bdbcur -> string
 
+  (** [with_cursor db f] DOCUMENT_ME *)
   val with_cursor : bdb -> (bdb -> bdbcur -> 'a) -> 'a
 
   val _tranbegin: bdb -> unit
@@ -117,6 +157,7 @@ module Bdb : sig
   type flag = BDBFOPEN | BDBFFATAL
   val flags: bdb -> flag list
 
+  (** [defrag ~step db] DOCUMENT_ME *)
   val defrag : ?step:int64 -> bdb -> int
 
   val copy_from_cursor : source:bdb -> cursor:bdbcur -> target:bdb -> max:int option -> int
